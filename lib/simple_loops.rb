a151a6e9bdec0982cc1c2285b4ed9807f19679fc
Jake Peterson <jpeterson0514@gmail.com>
2020-07-23 21:59:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,6 +14,13 @@ def loop_message_n_times(string,integer)
   end
 end 
 
+def output_array(array)
+  count = 0 
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end 
+end 
   
   
   
